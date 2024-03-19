a3b7ddeabe7ce57f91134584debf28725e1513fc
Dan Kiuna <dankiuna@gmail.com>
2024-03-19 17:14:10 -0500

    changes

---
+++ src/app/AmplifyAPI.service.ts
@@ -0,0 +1,162 @@
+import { Injectable } from '@angular/core';
+import { generateClient } from 'aws-amplify/data';
+import * as AmplifyTypes from './API.service';
+import * as queries from '../graphql/queries';
+import * as mutations from '../graphql/mutations';
+import * as subscriptions from '../graphql/subscriptions';
+
+import { Observable } from 'rxjs';
+
+const client = generateClient();
+
+@Injectable({
+  providedIn: 'root',
+})
+export class APIService {
+  async CreateTodo(
+    input: AmplifyTypes.CreateTodoInput, //CreateTodoInput,
+    condition?: AmplifyTypes.ModelTodoConditionInput
+  ): Promise<AmplifyTypes.CreateTodoMutation> {
+    const result = await client.graphql({
+      query: mutations.createTodo,
+      variables: {
+        input: input,
+        condition: condition ? condition : null,
+      },
+    });
+
+    return <AmplifyTypes.CreateTodoMutation>result.data.createTodo;
+  }
+
+  async UpdateTodo(
+    input: AmplifyTypes.UpdateTodoInput,
+    condition?: AmplifyTypes.ModelTodoConditionInput
+  ): Promise<AmplifyTypes.UpdateTodoMutation> {
+    const result = await client.graphql({
+      query: mutations.updateTodo,
+      variables: {
+        input: input,
+        condition: condition ? condition : null,
+      },
+    });
+    return <AmplifyTypes.UpdateTodoMutation>result.data.updateTodo;
+  }
+  async DeleteTodo(
+    input: AmplifyTypes.DeleteTodoInput,
+    condition?: AmplifyTypes.ModelTodoConditionInput
+  ): Promise<AmplifyTypes.DeleteTodoMutation> {
+    const result = await client.graphql({
+      query: mutations.deleteTodo,
+      variables: {
+        input: input,
+        condition: condition ? condition : null,
+      },
+    });
+    return <AmplifyTypes.DeleteTodoMutation>result.data.deleteTodo;
+  }
+
+  async GetTodo(id: string): Promise<AmplifyTypes.GetTodoQuery> {
+    const result = await client.graphql({
+      query: queries.getTodo,
+      variables: {
+        id: id,
+      },
+    });
+    return <AmplifyTypes.GetTodoQuery>result.data.getTodo;
+  }
+  async ListTodos(
+    filter?: AmplifyTypes.ModelTodoFilterInput,
+    limit?: number,
+    nextToken?: string
+  ): Promise<AmplifyTypes.ListTodosQuery> {
+    const result = await client.graphql({
+      query: queries.listTodos,
+      variables: {
+        filter: filter ? filter : null,
+        limit: limit ? limit : null,
+        nextToken: nextToken ? nextToken : null,
+      },
+    });
+    return result.data;
+  }
+  OnCreateTodoListener(
+    filter?: AmplifyTypes.ModelSubscriptionTodoFilterInput,
+    owner?: string
+  ): Observable<AmplifyTypes.OnCreateTodoSubscription> {
+    return client.graphql({
+      query: subscriptions.onCreateTodo,
+      variables: {
+        filter: filter ? filter : null,
+        owner: owner ? owner : null,
+      },
+    }) as Observable<AmplifyTypes.OnCreateTodoSubscription>;
+    // .subscribe({
+    //   next: (event: any) => {
+    //     console.log('event', event);
+    //   },
+    //   complete: () => console.log('complete', event),
+    // });
+  }
+
+  // OnUpdateTodoListener(
+  //   filter?: ModelSubscriptionTodoFilterInput,
+  //   owner?: string
+  // ): Observable<
+  //   SubscriptionResponse<Pick<__SubscriptionContainer, 'onUpdateTodo'>>
+  // > {
+  //   const statement = `subscription OnUpdateTodo($filter: ModelSubscriptionTodoFilterInput, $owner: String) {
+  //       onUpdateTodo(filter: $filter, owner: $owner) {
+  //         __typename
+  //         id
+  //         name
+  //         description
+  //         createdAt
+  //         updatedAt
+  //         owner
+  //       }
+  //     }`;
+  //   const gqlAPIServiceArguments: any = {};
+  //   if (filter) {
+  //     gqlAPIServiceArguments.filter = filter;
+  //   }
+  //   if (owner) {
+  //     gqlAPIServiceArguments.owner = owner;
+  //   }
+  //   return API.graphql(
+  //     graphqlOperation(statement, gqlAPIServiceArguments)
+  //   ) as Observable<
+  //     SubscriptionResponse<Pick<__SubscriptionContainer, 'onUpdateTodo'>>
+  //   >;
+  // }
+
+  // OnDeleteTodoListener(
+  //   filter?: ModelSubscriptionTodoFilterInput,
+  //   owner?: string
+  // ): Observable<
+  //   SubscriptionResponse<Pick<__SubscriptionContainer, 'onDeleteTodo'>>
+  // > {
+  //   const statement = `subscription OnDeleteTodo($filter: ModelSubscriptionTodoFilterInput, $owner: String) {
+  //       onDeleteTodo(filter: $filter, owner: $owner) {
+  //         __typename
+  //         id
+  //         name
+  //         description
+  //         createdAt
+  //         updatedAt
+  //         owner
+  //       }
+  //     }`;
+  //   const gqlAPIServiceArguments: any = {};
+  //   if (filter) {
+  //     gqlAPIServiceArguments.filter = filter;
+  //   }
+  //   if (owner) {
+  //     gqlAPIServiceArguments.owner = owner;
+  //   }
+  //   return API.graphql(
+  //     graphqlOperation(statement, gqlAPIServiceArguments)
+  //   ) as Observable<
+  //     SubscriptionResponse<Pick<__SubscriptionContainer, 'onDeleteTodo'>>
+  //   >;
+  // }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { APIService } from './API.service';
-import { Auth } from 'aws-amplify';
+import { APIService } from './AmplifyAPI.service';
+import { signIn } from 'aws-amplify/auth';
+import { Observable } from 'rxjs';
+import { OnCreateTodoSubscription } from './API.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,10 +11,18 @@ import { Auth } from 'aws-amplify';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-amplified-services';
+export class AppComponent implements OnInit {
+  public title = 'angular-amplified-services';
+  // private subscription: Observable<OnCreateTodoSubscription> | null = null;
+  private api = new APIService();
 
-  api = new APIService();
+  async ngOnInit() {
+    this.api.OnCreateTodoListener().subscribe({
+      next: (e) => console.log('next', e),
+      // complete: e => console.log('complete',e),
+      // error: console.log('Error')
+    });
+  }
 
   public async getData() {
     try {
@@ -23,9 +33,20 @@ export class AppComponent {
     }
   }
 
+  public async createData() {
+    try {
+      const result = await this.api.CreateTodo({
+        name: 'A todo' + Date.now(),
+      });
+      console.log('Created', result);
+    } catch (e) {
+      console.log('SOme error creating', e);
+    }
+  }
+
   public async signIn() {
     try {
-      const user = await Auth.signIn({
+      const user = await signIn({
         username: 'dkkiuna11@gmail.com',
         password: 'abcd1234',
       });
